b80526946dcc8198d2bc8fdec3b4532d1f7feb98
Aniket <aniketrandhave15@gmail.com>
2021-10-14 11:27:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
           <Navbar />
 
           <Switch>
-            {/* <Route exact path='randhaveAniket' component={Home} /> */}
+            <Route exact path='randhaveAniket' component={Home} />
             <Route exact path='/' component={Home} />
             <Route exact path='/home' component={Home} />
             <Route exact path='/service' component={Services} />
